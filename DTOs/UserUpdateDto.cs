d0a138f0338403e5cd9284ec12b908f97b48550a
LeLong0606 <lelong.06062002@gmail.com>
2025-08-07 23:23:08 +0700

    1.2

--- DTOs/UserUpdateDto.cs
+++ DTOs/UserUpdateDto.cs
@@ -8,6 +8,7 @@ namespace ShiftManagementFE.DTOs
     /// </summary>
     public class UserUpdateDto
     {
+        public string Username { get; set; } = string.Empty;
         public string FullName { get; set; } = string.Empty;
         public string? Email { get; set; }
         public string? PhoneNumber { get; set; }
