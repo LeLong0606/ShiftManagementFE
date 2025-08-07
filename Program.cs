3a18bbb82dc259f6e84c0aa7065ebb892e023be2
LeLong0606 <lelong.06062002@gmail.com>
2025-08-07 22:57:23 +0700

    1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,27 @@
 using ShiftManagementFE.Components;
+using ShiftManagementFE.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+// Register HttpClient for dependency injection
+builder.Services.AddHttpClient();
+
+// Register application services for DI
+builder.Services.AddScoped<UserService>();
+builder.Services.AddScoped<AuthService>();
+builder.Services.AddScoped<DepartmentService>();
+builder.Services.AddScoped<HolidayService>();
+builder.Services.AddScoped<StoreService>();
+builder.Services.AddScoped<RoleService>();
+builder.Services.AddScoped<LogService>();
+builder.Services.AddScoped<ScheduleService>();
+builder.Services.AddScoped<AttendanceService>();
+builder.Services.AddScoped<ExportService>();
+builder.Services.AddScoped<UserRoleService>();
 
 var app = builder.Build();
 
